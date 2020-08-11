6dd318528f146c5ca3b849344a1b20f58d7d0c5e
Leandro Esteba Gallippi <leandro.gallippi@rappi.com>
2020-08-11 09:41:47 -0300

    style: centering modal

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,21 +1,15 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './Modal.scss';
 
 export function Modal({ show, children, onClose }) {
-  const node = useRef(null);
-
-  function handleClick(event) {
-    if (event.target.className === node.current.className) {
-      onClose();
-    }
-  }
-
   return (
-    <div className={`modal${show ? '' : ' modal--hidden'}`} ref={node} onClick={handleClick}>
-      <div className="modal__content">
+    <>
+      <div className={`modal${show ? '' : ' modal--hidden'}`}>
         {show ? children : null}
       </div>
-      <span className="modal__close" onClick={onClose}>&times;</span>
-    </div>
+      { show && <div className="modal-overlay" onClick={onClose}>
+          <span className="modal-overlay__close" onClick={onClose}>&times;</span>
+        </div>}
+    </>
   );
 }
